fe8bd67964d3d1d197089ec0b3833dc475a45646
Nikita Martin <nikitamartin2003@icloud.com>
2018-08-07 15:53:23 +0100

    create reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180807091802) do
+ActiveRecord::Schema.define(version: 20180807141952) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
